0546c64b1200bd792c7d4c244ae9b29260a0a0bf
DigitalDruid <playerskingdom@hotmail.com>
2024-11-28 15:44:59 -0800

    added additional setion to lisst_tuples_sets

--- list_tuples_sets.py
+++ list_tuples_sets.py
@@ -22,36 +22,101 @@ print(courses[2:])  # ['Physics', 'CompSci'] (index 2 to end) (third to last ele
 
 # Add an element to the end of the list (append method)
 
-courses.append('Art')  # add an element to the end of the list (append method)
+courses2 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+courses2.append('Art')  # add an element to the end of the list (append method)
 
 print(courses)  # ['History', 'Math', 'Physics', 'CompSci', 'Art'] (Art added to the end of the list)
 
 # Add an element to a specific position in the list (insert method)
 
-courses.insert(0, 'Art')  # add an element to the beginning of the list (insert method)
+courses3 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
 
-print(courses)  # ['Art', 'History', 'Math', 'Physics', 'CompSci', 'Art'] (Art added to the beginning of the list)
+courses3.insert(0, 'Art')  # add an element to the beginning of the list (insert method)
+
+print(courses)  # ['Art', 'History', 'Math', 'Physics', 'CompSci'] (Art added to the beginning of the list)
+
+courses4 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
 
 courses_2 = ['Art', 'Education']  # list (square brackets)
 
-courses.insert(0, courses_2)  # add a list to the beginning of the list (insert method)
+courses4.insert(0, courses_2)  # add a list to the beginning of the list (insert method)
 
-print(courses)  # [['Art', 'Education'], 'Art', 'History', 'Math', 'Physics', 'CompSci', 'Art'] (list added to the beginning of the list)
+print(courses4)  # [['Art', 'Education'], 'History', 'Math', 'Physics', 'CompSci'] (list added to the beginning of the list)
 
-print(courses[0])  # ['Art', 'Education'] (index 0) (first element) (position 1) (start)
+print(courses4[0])  # ['Art', 'Education'] (index 0) (first element) (position 1) (start)
 
-print(courses[0][1])  # Education (index 0, 1) (first element, second element) (position 1, 2) (start, next)
+print(courses4[0][1])  # Education (returns the second element of the first element of the list) (index 0, 1) (first element, second element) (position 1, 2) (start, next)
 
 # Add multiple elements to the end of the list (extend method)
 
-courses.extend(courses_2)  # add multiple elements to the end of the list (extend method)
+courses4.extend(courses_2)  # add multiple elements to the end of the list (extend method)
 
-print(courses)  # [['Art', 'Education'], 'Art', 'History', 'Math', 'Physics', 'CompSci', 'Art', 'Art', 'Education'] (list added to the end of the list)
+print(courses)  # ['History', 'Math', 'Physics', 'CompSci', 'Art', 'Education'] (list added to the end of the list)
 
 # Remove an element from the list (remove method)
 
-courses.remove('Math')  # remove an element from the list (remove method)
+courses4.remove('Math')  # remove an element from the list (remove method)
 
-print(courses)  # [['Art', 'Education'], 'Art', 'History', 'Physics', 'CompSci', 'Art', 'Art', 'Education'] (Math removed from the list)
+print(courses4)  # ['History', 'Physics', 'CompSci'] (Math removed from the list)
 
-# cont at 9:05 in the video
+courses5 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+courses5.pop()  # remove the last element from the list (pop method)
+
+print(courses5) # 'History', 'Math', 'Physics'(Compsci removed from the list)
+
+popped = courses5.pop()  # remove the last element from the list and assign it to a variable (popped method)    
+
+print(popped)  # Compsci (Compsci removed from the list and assigned to the variable) 
+
+# Reverse the order of the list (reverse method)
+
+courses6 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+courses6.reverse()  # reverse the order of the list (reverse method)
+
+print(courses6)  # ['CompSci', 'Physics', 'Math', 'History'] (list reversed)
+
+# Sort the list in ascending order (sort method)    
+
+courses7 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+courses7.sort()  # sort the list in ascending order (sort method)
+
+nums = [1, 5, 3, 4, 2]  # list (square brackets)
+
+nums.sort()  # sort the list in ascending order (sort method)
+
+print(nums)  # [1, 2, 3, 4, 5] (list sorted in ascending order)
+
+print(courses7)  # ['CompSci', 'History', 'Math', 'Physics'] (list sorted in ascending order)
+
+# Sort the list in descending order (sort method)
+
+courses8 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+nums2 = [1, 5, 3, 4, 2]  # list (square brackets)
+
+nums2.sort(reverse=True)  # sort the list in descending order (sort method) 
+
+print(nums2)  # [5, 4, 3, 2, 1] (list sorted in descending order)
+
+courses8.sort(reverse=True)  # sort the list in descending order (sort method)
+
+print(courses8)  # ['Physics', 'Math', 'History', 'CompSci'] (list sorted in descending order)
+
+# Sort the list in ascending order without changing the original list (sorted function)
+
+courses9 = ['History', 'Math', 'Physics', 'CompSci']  # list (square brackets)
+
+sorted_courses = sorted(courses9)  # sort the list in ascending order without changing the original list (sorted function) must be assigned to a variable
+
+print(sorted_courses)  # ['CompSci', 'History', 'Math', 'Physics'] (list sorted in ascending order)
+
+print(courses9)  # ['History', 'Math', 'Physics', 'CompSci'] (original list unchanged)
+
+# Sort the list in descending order without changing the original list (sorted function)
+
+# cont. video 13:40
+
